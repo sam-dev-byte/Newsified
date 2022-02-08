eb382c6e5e2ff5d10935619c0273f81203d325fe
Gift kipkemboi <gmansesco@gmail.com>
2022-02-08 03:25:51 -0500

    wrote an article test function

--- tests/test_article.py
+++ tests/test_article.py
@@ -1,6 +1,10 @@
 import unittest
 from app.models import Article
 
+class ArticleTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Article class
+    '''
 
     def setUp(self):
         '''
