ad74787c7406880ab34dd58867f232268e631c5f
Gift kipkemboi <gmansesco@gmail.com>
2022-02-08 06:12:35 -0500

    deployed  to heroku

--- tests/test_category.py
+++ tests/test_category.py
@@ -9,3 +9,4 @@ class categoryTest(unittest.TestCase):
         method that will run before every test
         '''
         self.new_category = Category('Naila-Jean Meyers and Scott Cacciola','In the 30th professional match between the sisters, Serena defeated Venus in the third round. Rafael Nadal, Juan Martin del Potro and Sloane Stephens also won Friday.','2018-09-01T05:22:26Z','https://www.nytimes.com/2018/08/31/sports/tennis/us-open-results.html','https://static01.nyt.com/images/2018/09/01/sports/01openlive7/01openlive7-facebookJumbo.jpg','US Open 2018 Results')
+        self.assertTrue(isinstance(self.new_category,Category))
