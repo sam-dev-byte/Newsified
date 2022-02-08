a13bf738eed69d978129882671648c91355079da
Gift kipkemboi <gmansesco@gmail.com>
2022-02-08 03:07:55 -0500

    initialised the flask and bootstrap extensions

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 
+def create_app(config_name):
 
 
 
     # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+    
 
     # Initializing flask extensions
     #bootstrap.init_app(app)
